4615f947c1b54890ebb98eb6b2d02ea24102d306
matthieuTa <matthieu.tabarin@gmail.com>
2019-05-28 15:01:36 +0200

    Add secure_asset for assets

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,5 @@
 <p>Footer</p>
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('service-worker.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
