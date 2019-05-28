8137742390beff86c2a2107a516863eefc608b3a
matthieuTa <matthieu.tabarin@gmail.com>
2019-05-28 14:27:28 +0200

    Add PWA files

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,15 @@ files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(
 const app = new Vue({
     el: '#app',
 });
+
+if ('serviceWorker' in navigator ) {
+  window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
