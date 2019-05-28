a0f5bc4072f9c5821b0e623ca89d17f8e54539cd
matthieuTa <matthieu.tabarin@gmail.com>
2019-05-28 15:04:33 +0200

    fix broken link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <p>Footer</p>
-<script type="text/javascript" src="{{ secure_asset('service-worker.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/service-worker.js') }}"></script>
 <script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
