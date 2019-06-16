d9b615870b659a8e29d5b84e7d7bd490d1859698
matthieuTa <matthieu.tabarin@gmail.com>
2019-06-16 23:41:52 +0200

    Created authentification

---
+++ src/views/Authenticate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <h1>Authenticate</h1>
+    <Login></Login>
+    <Register></Register>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
+export default {
+  components: {
+    Login,
+    Register
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
