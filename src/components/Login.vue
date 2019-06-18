9c428427d425be0c1ffe173447dbb246968ee3de
matthieuTa <matthieu.tabarin@gmail.com>
2019-06-18 23:50:18 +0200

    Add admin first components

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    <h1>Login</h1>
+      <form>
+        <input type="text" name="email" placeholder="Email" v-model="email">
+        <input type="password" name="password" placeholder="Password" v-model="password">
+        <button type="button" name="button" @click="login()">Je m'inscris</button>
+      </form>
+
+  </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      Firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        function(user){
+          alert('your account has been created')
+        },
+        function(err){
+          alert('Ooops... ' + err.message )
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/admin/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="">
+  <h1>Admin dashboard</h1>
+  <p>Big stuff happens here</p>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
